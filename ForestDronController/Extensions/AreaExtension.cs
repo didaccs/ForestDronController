77ad72f272c204b1b289fe7ab2039263d6277f2d
Didac <didac.campanals.ext@suez.com>
2019-02-21 19:18:41 +0100

    Comment classes and fix Test Location

--- ForestDronController/Extensions/AreaExtension.cs
+++ ForestDronController/Extensions/AreaExtension.cs
@@ -5,13 +5,27 @@ using System.Linq;
 
 namespace ForestDronController.Extensions
 {
+    /// <summary>
+    /// Extension class for Parse Area object
+    /// </summary>
     public static class AreaExtension
     {
+        /// <summary>
+        /// Parse Area to string with format 0 0
+        /// </summary>
+        /// <param name="area"></param>
+        /// <returns></returns>
         public static String ToStringArea(this Area area)
         {
             return String.Format("{0}x{1}", area.X, area.Y);
         }
 
+        /// <summary>
+        /// Parse string to Area. 
+        /// In case that the string is not correct throw an InvalidAreaException
+        /// </summary>
+        /// <param name="area"></param>
+        /// <returns></returns>
         public static Area ParseArea(this string area)
         {
             // Check string length
--- ForestDronController/Extensions/LocationExtension.cs
+++ ForestDronController/Extensions/LocationExtension.cs
@@ -1,20 +1,31 @@
 ﻿using ForestDronController.Entities;
 using ForestDronController.Exceptions;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
 
 namespace ForestDronController.Extensions
 {
+    /// <summary>
+    /// Extension class for Parse Location object
+    /// </summary>
     public static class LocationExtension
     {
+        /// <summary>
+        /// Parse Location to string with format 0 0 E
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
         public static String ToStringLocation(this Location location)
         {
             return String.Format("{0} {1} {2}", location.X, location.Y, (char)location.Direction);
         }
 
+        /// <summary>
+        /// Parse string to Location. 
+        /// In case that the string is not correct throw an InvalidLocationException
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
         public static Location ParseLocation(this string location)
         {
             // Check string length
@@ -32,9 +43,14 @@ namespace ForestDronController.Extensions
                 throw new InvalidLocationException(location);
             }
 
+            //Check character value
+            if (!Direction.TryParse(typeof(Direction), ((int)locationChars[4]).ToString(), out object dir))
+            {
+                throw new InvalidLocationException(location);
+            }
+
             double xLocation = Char.GetNumericValue(locationChars[0]);
             double yLocation = Char.GetNumericValue(locationChars[2]);
-            Enum.TryParse(typeof(Direction), locationChars[4].ToString(), out object dir);
 
             //check number values
             if (xLocation <= 0 || yLocation <= 0 || xLocation >= int.MaxValue || yLocation >= int.MaxValue || dir == null)
